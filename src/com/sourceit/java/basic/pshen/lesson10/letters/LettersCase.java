2b68c1fcb6b2870a98992fc9707fd08725f9900f
Nasredin-li <@user-3f23ea9bf0>
2015-04-28 22:03:58 +0300

    Letter Case

---
+++ src/com/sourceit/java/basic/pshen/lesson10/letters/LettersCase.java
@@ -0,0 +1,16 @@
+package com.sourceit.java.basic.pshen.lesson10.letters;
+
+public class LettersCase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String s = args[1];
+		char[] ch = s.toCharArray();
+		
+		System.out.println(ch);
+
+	}
+
+}
