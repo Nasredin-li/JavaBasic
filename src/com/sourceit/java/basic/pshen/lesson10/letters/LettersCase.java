203c91d3ec707461e1763591712f5331a8349a36
designer7 <s.prepress@yandex.ru>
2015-05-06 14:19:22 +0300

    Finish

--- src/com/sourceit/java/basic/pshen/lesson10/letters/LettersCase.java
+++ src/com/sourceit/java/basic/pshen/lesson10/letters/LettersCase.java
@@ -6,11 +6,26 @@ public class LettersCase {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String s = args[1];
-		char[] ch = s.toCharArray();
-		
-		System.out.println(ch);
 
+		for (int i = 0; i < args.length; i++) {
+			String s = args[i];
+			int j = i + 1;
+			if (j < args.length) {
+				String d = args[j];
+				if (s.contains(".") == false) {
+					System.out.print(args[i] + " ");
+				} else {
+					System.out.println(args[i]);
+					char[] ch = d.toUpperCase().toCharArray();
+					System.out.print(ch[0]);
+					System.out.print(d.substring(1));
+					System.out.print(" ");
+					i++;
+				} 
+			}else {
+				System.out.println(args[i] + " ");
+				}
+		}
 	}
 
 }
