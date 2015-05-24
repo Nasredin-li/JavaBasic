c07b029bf59d290191b24e6505ad9d3b80c504f4
Sergey <s.prepress@yandex.ru>
2015-05-25 01:03:01 +0300

    ht 14 Exceptions Not ready yet

--- src/com/sourceit/java/basic/pshen/ht14/Exeptions.java
+++ src/com/sourceit/java/basic/pshen/ht14/Exeptions.java
@@ -2,13 +2,36 @@ package com.sourceit.java.basic.pshen.ht14;
 
 public class Exeptions {
 
+	@SuppressWarnings("finally")
 	public static void main(String[] args) {
-		String str = new String(args[0]);
-		System.out.println(str);
+		String str= new String(args[0]);
+		//System.out.println(str);
 		try {
+			str = new String(args[0]);
+			
 			System.out.print(str.substring(Integer.parseInt(args[1]),
 					Integer.parseInt(args[2])));
-			// throw new NumberFormatException();
+			// throw new StringIndexOutOfBoundsException();
+		} catch (NumberFormatException nfe) {
+			try {
+				System.out.print(str.substring(Integer.parseInt(args[1])));
+				throw new StringIndexOutOfBoundsException();
+			} catch (NumberFormatException nfe2) {
+				try {
+
+					System.out.print(str.substring(Integer.parseInt(args[2])));
+					
+				} catch (NumberFormatException nfe3) {
+					System.out
+							.print(" Enter one or two digit, to define a print range substring");
+				} catch (StringIndexOutOfBoundsException nfe3) {
+					System.out
+							.print(" Enter one or two digit, to define a print range substring");
+				} catch (ArrayIndexOutOfBoundsException ok) {
+					System.out
+					.print(" Enter one or two digit, to define a print range substring");
+				}
+			}
 
 		} catch (ArrayIndexOutOfBoundsException ai) {
 			try {
@@ -25,7 +48,7 @@ public class Exeptions {
 		} catch (StringIndexOutOfBoundsException si) {
 			try {
 				System.out.print(str.substring(Integer.parseInt(args[1])));
-				//throw new NumberFormatException();
+				// throw new NumberFormatException();
 			} catch (StringIndexOutOfBoundsException sio) {
 				try {
 					System.out.print(str.substring(Integer.parseInt(args[2])));
@@ -35,18 +58,6 @@ public class Exeptions {
 				}
 			}
 
-		} catch (NumberFormatException nfe) {
-			try {
-				System.out.print(str.substring(Integer.parseInt(args[1])));
-			} catch (NumberFormatException nfe2) {
-				try {
-					System.out.print(str.substring(Integer.parseInt(args[2])));
-					
-				} catch (NumberFormatException nfe3) {
-					System.out
-							.print(" Enter one or two digit, to define a print range substring");
-				}
-			}
 		}
 	}
 }
