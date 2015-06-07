9791372c9c1fe329b388d8c54cbc61276745ea12
Sergey <s.prepress@yandex.ru>
2015-06-08 01:34:06 +0300

    HT 17. jar. Not ready

---
+++ src/com/sourceit/java/basic/pshen/ht17/Activator.java
@@ -0,0 +1,25 @@
+package com.sourceit.java.basic.pshen.ht17;
+
+import java.util.ArrayList;
+import com.sourceit.java.basic.pshen.ht6.Play;
+import com.sourceit.java.basic.pshen.ht7.Exer;
+import com.sourceit.java.basic.pshen.ht8.Launcher2;
+import com.sourceit.java.basic.pshen.ht10.Scenario;
+import com.sourceit.java.basic.pshen.ht10.Scenario;
+
+
+public class Activator {
+
+	public static void main(String[] args) {
+		//ArrayList<String> print = new ArrayList<String>();
+		com.sourceit.java.basic.pshen.ht6.Play.main(args);
+		com.sourceit.java.basic.pshen.ht7.Exer.main(args);
+		com.sourceit.java.basic.pshen.ht8.Launcher2.main(args);
+		com.sourceit.java.basic.pshen.ht10.Scenario.main(args);
+		com.sourceit.java.basic.pshen.ht10.Scenario.main(args);
+	}
+	void print(){
+		
+	}
+
+}
